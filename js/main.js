bd67564a426b453dcead08a778bada536c773ce9
kdrbipul <kdrbipul@gmail.com>
2023-06-22 19:09:48 +0600

    code all done

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ const searchBtn = body.querySelector(".search-box");
 const modeSwitch = body.querySelector(".toggle-switch");
 const modeText = body.querySelector(".mode-text");
 
-toggle.addEventListener("click", =>{
+toggle.addEventListener("click", ()=>{
     sidebar.classList.toggle("close")
 })
-modeSwitch.addEventListener("click", =>{
+modeSwitch.addEventListener("click", ()=>{
     body.classList.toggle("dark")
 });
